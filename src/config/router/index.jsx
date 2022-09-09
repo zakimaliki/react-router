d190a3c8c18e4330bff617635748839fd246e15d
zakimaliki-macbook <Zaki@MacintoshHDsAir.lan>
2022-09-09 22:30:51 +0700

    new

---
+++ src/config/router/index.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { BrowserRouter, Link, Navigate, Route, Routes } from "react-router-dom";
+import About from '../../pages/About';
+import Detail from '../../pages/Detail';
+import Home from '../../pages/Home';
+import Page404 from '../../pages/Page404';
+
+
+const Router = () => {
+  return (
+    <div>
+    <BrowserRouter>
+        <ul>
+          <li>
+            <Link to="/home">Home</Link>
+          </li>
+          <li>
+            <Link to="about">About</Link>
+          </li>
+          <li>
+            <Link to="/product/1">Detail</Link>
+          </li>
+        </ul>
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" replace="true" />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/product/:id" element={<Detail />} />
+          <Route path="*" element={<Page404 />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default Router
---
+++ src/pages/Detail.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+const Detail = () => {
+    const {id} = useParams();
+    return (
+    <div>Detail Page {id}</div>
+  )
+}
+
+export default Detail
